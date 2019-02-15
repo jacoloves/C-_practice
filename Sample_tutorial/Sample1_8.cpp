d6d5652c3f17f84226f2b55f2a887813c4f01d52
jacoloves <5511068t@gmail.com>
2019-02-15 14:41:47 +0000

    20190215

---
+++ Sample_tutorial/Sample1_8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int name;
+
+   name = 10;
+
+   cout << name << endl;
+   cout << name + 2 << endl;
+   cout << name + 1 << endl;
+}
+
